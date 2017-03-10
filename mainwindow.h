96f5ea631ea0b44f4947f4997fbfb88fe819d61f
Jaime Quiroga <gtronick@gmail.com>
2017-03-10 15:40:49 -0500

    Deleted custom buttons

--- mainwindow.h
+++ mainwindow.h
@@ -13,26 +13,15 @@ class MainWindow : public QMainWindow
     Q_OBJECT
     QThread workerThread;
 public:
-    bool eventFilter(QObject *obj, QEvent *event);
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
 private slots:
     void on_startButton_clicked();
-
     void on_resetButton_clicked();
-
     void on_flagButton_clicked();
-
     void workerFinish(QString data);
-
     void on_clearLapsButton_clicked();
-
-    void on_closeWindowButton_clicked();
-
-    void on_restoreWindowButton_clicked();
-
-    void on_restoreWindowButton_2_clicked();
 
 signals:
     void startChrono();
@@ -40,11 +29,6 @@ signals:
     void killChronoThread();
 
 private:
-    void mousePressEvent(QMouseEvent *event);
-    void mouseMoveEvent(QMouseEvent *event);
-
-    int m_nMouseClick_X_Coordinate;
-    int m_nMouseClick_Y_Coordinate;
     Ui::MainWindow *ui;
     int startFlag;
     int stopFlag;
